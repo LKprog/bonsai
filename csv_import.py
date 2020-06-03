d1dd6d9c613790c2e621a2d08d19ffb9e67fd773
Lieke <liekekollen@hotmail.com>
2020-06-03 12:58:38 +0200

    initial csv load

--- csv_import.py
+++ csv_import.py
@@ -1,5 +1,21 @@
-f = open("ConnectiesHolland.csv")
-connecties = csv.reader(f)
+import csv
 
-f = open("StationsHolland.csv")
-stations = csv.reader(f)
+class station():
+
+    def__init__(self, station, x, y):
+        self._station = station
+        self._x = x
+        self._y = y
+
+        f = open("StationsHolland.csv")
+        stations = csv.reader(f)
+
+class connection():
+    
+    def__init__(self, station1, station2, duration):
+        self._station1 = station1
+        self._station2 = station2
+        self._duration = duration
+
+    f = open("ConnectiesHolland.csv")
+    connecties = csv.reader(f)
