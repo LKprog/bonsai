7387ad880ed8b0120618a348f1dab6f71c36fd06
Lieke <liekekollen@hotmail.com>
2020-06-05 17:13:24 +0200

    traject class

--- code/classes/traject.py
+++ code/classes/traject.py
@@ -1,3 +1,23 @@
+from station import Station
+
 class Traject():
-    def __init__(self):
-        pass
+    def __init__(self, traject_id, current_station):
+        self.traject_id = traject_id
+        self.current_station = current_station
+        self.trajects = []
+        self.total_distance = 
+        
+    def get_connection(self, current_station):
+        return self.connections_list.get(current_station)
+
+    def add_connection(self, next_station):
+        self.traject.append(next_station[0])
+        self.total_distance += next_station[1]
+    
+    def move(self):
+        self.current_station = self.next_station
+
+    def stop(self):
+        for traject in self.trajects:
+            if traject
+
