3bbea07abc909f1df20ada799c84dafb6cb3ba62
Lieke <liekekollen@hotmail.com>
2020-06-08 10:31:46 +0200

    longitude and latitude juist voor kaart

--- code/visualisation/visualise.py
+++ code/visualisation/visualise.py
@@ -6,32 +6,33 @@ from bokeh.models import ColumnDataSource, Label, LabelSet
 
 # prepare some data
 
-with open(sys.argv[1], 'r') as input_file:
+with open('StationsHolland.csv', 'r') as input_file:
     reader = csv.DictReader(input_file)
-    csv_x = []
-    csv_y = []
+    csv_longitude = []
+    csv_latitude = []
     stations = []
     for row in reader:
         stations.append(row['station'])
-        csv_x.append(float(row['x']))
-        csv_y.append(float(row['y']))
+        csv_longitude.append(float(row['x']))
+        csv_latitude.append(float(row['y']))
 N = 4000
 
-x = np.array(csv_x)
-y = np.array(csv_y)
+longitude = np.array(csv_longitude)
+latitude = np.array(csv_latitude)
 
-source = ColumnDataSource(data=dict(x=x, y=y, stations=stations))
+
+source = ColumnDataSource(data=dict(latitude=latitude, longitude=longitude, stations=stations))
 # output to static HTML file (with CDN resources)
 output_file("color_scatter.html", title="color_scatter.py example", mode="cdn")
 
 TOOLS = "wheel_zoom,box_zoom,reset,box_select,lasso_select"
 
 # create a new plot with the tools above, and explicit ranges
-p = figure(tools=TOOLS, x_range=(51.5, 53.5), y_range=(4.2, 5.2))
+p = figure(tools=TOOLS, y_range=(51.5, 53.5), x_range=(4.2, 5.2))
 font = 1
 # add a circle renderer with vectorized colors and sizes
-p.circle(x, y)
-labels = LabelSet(x='x', y='y', text='stations', text_font_size='7pt', level='glyph',
+p.circle(latitude, longitude)
+labels = LabelSet(x='latitude', y='longitude', text='stations', text_font_size='7pt', level='glyph',
               x_offset=5, y_offset=5, source=source, render_mode='canvas')
 p.add_layout(labels)
 
