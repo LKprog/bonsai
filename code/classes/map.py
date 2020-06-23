5984ccfbeb5fad399b011dec9abd583f4e67c16d
WillemHenkelman <willem.henkelman@gmail.com>
2020-06-23 20:39:52 +0200

    added whitespace below docstrings

--- code/classes/map.py
+++ code/classes/map.py
@@ -4,7 +4,7 @@
  * Minor programming Universiteit van Amsterdam - Programmeertheorie - RailNL
  * Daphne Westerdijk, Willem Henkelman, Lieke Kollen
  *
- * This code loads all the stations and its connections and duration from the data files provided and it can be regarded as a literal map 
+ * This code loads all the stations and its connections and duration from the data files provided and it can be regarded as a literal map
  """
 
 import csv
@@ -20,6 +20,7 @@ class Map:
         """
         method that initializes the map, loads the stations objects and connections and makes a dict that contains all connections
         """
+
         self.stations = self.load_stations(station_file)
         self.connections = self.load_connections(connection_file)
         self.all_connections = {}
@@ -28,12 +29,13 @@ class Map:
         """
         method that creates a Station object for each of the stations and its coordinates in the station_file
         """
+
         # create stations dict
         stations = {}
 
         # open the station_file
         with open(source_file, 'r') as input_file:
-            
+
             # read the data per row and transfer it to the dict
             reader = csv.DictReader(input_file)
             for row in reader:
@@ -46,12 +48,14 @@ class Map:
         """
         method that creates connections for the stations object in both directions based on the connections_file
         """
-        # open the connections_file and 
+
+        # open the connections_file and
         with open(source_file, 'r') as input_file:
-           
+
             # read the data per row
             reader = csv.DictReader(input_file)
             for row in reader:
+
                 # add the connection and duration to both the start station object and end station object of the connection, as a train can run both ways
                 station = self.stations[row['station1']]
                 station_reversed = self.stations[row['station2']]
--- code/classes/station.py
+++ code/classes/station.py
@@ -11,11 +11,12 @@ class Station:
     """
     station class that loads the stations and its corresponding connections, it also tracks the station's unused connections
     """
-    
+
     def __init__(self, name, x, y):
         """
         method to initialize the class and its properties
         """
+
         self.name = name
         self.x = x
         self.y = y
@@ -26,16 +27,19 @@ class Station:
         """
         method to append the connection with name and duration to the stations connection list
         """
+
         self.connections.append([next_station, duration])
 
     def add_unused_connection(self, next_station, duration):
         """
         method to append the unused connections of the station to the unused connections list
         """
+
         self.unused_connections.append([next_station, duration])
 
     def __repr__(self):
         """
         method to make sure that the object is printed properly if it is in a list/dic
         """
+
         return self.name
